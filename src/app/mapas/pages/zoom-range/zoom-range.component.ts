d24062c0bd5cf28e857efe0be0df045538569b58
gabopereyra <gabrielp1689@gmail.com>
2023-02-25 18:45:35 -0300

    ZoomRangeComponent modified, add button to add modify the zoom (less and more)

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
   styleUrls: ['./zoom-range.component.css']
 })
-export class ZoomRangeComponent implements OnInit {
+export class ZoomRangeComponent implements OnInit, AfterViewInit {
+  @ViewChild("mapa") mapaHtml! : ElementRef;
+
+  mapa!: mapboxgl.Map;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  
+  ngAfterViewInit(): void {
+    this.mapa = new mapboxgl.Map({
+      container: this.mapaHtml.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-60.631512318686006, -32.95502240399944],
+      zoom: 15
+    });
+  }
+
+  zoomIn(){
+    this.mapa.zoomIn();
+  }
+
+  zoomOut(){
+    this.mapa.zoomOut();
   }
 
 }
