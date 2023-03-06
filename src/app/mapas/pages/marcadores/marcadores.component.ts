72a050f0e5ba91ef97ce514973a9b1c4ddcfcbc2
gabopereyra <gabrielp1689@gmail.com>
2023-03-05 21:18:20 -0300

    method to add new markers. add structure in html to show new markers

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -1,6 +1,11 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 
+interface Marcador{
+  color: string;
+  marcador: mapboxgl.Marker
+}
+
 @Component({
   selector: 'app-marcadores',
   templateUrl: './marcadores.component.html',
@@ -9,8 +14,10 @@ import * as mapboxgl from 'mapbox-gl';
 export class MarcadoresComponent implements OnInit, AfterViewInit {
   @ViewChild("mapa") mapaHtml! : ElementRef;
   mapa!: mapboxgl.Map;
-  zoomLevel: number = 10;
+  zoomLevel: number = 15;
   coordenadas: [number, number] = [-60.631512318686006, -32.95502240399944];
+
+  marcadores : Marcador[] = []
 
   constructor() { }
 
@@ -30,4 +37,22 @@ export class MarcadoresComponent implements OnInit, AfterViewInit {
           .addTo(this.mapa);
   }
 
+  irMarcador(){
+    
+  }
+
+  agregarMarcador(){
+    const color = "#xxxxxx".replace(/x/g, y=>(Math.random()*16|0).toString(16));
+
+    const nuevo = new mapboxgl.Marker({
+      draggable: true,
+      color: color,
+    }).setLngLat(this.coordenadas)
+      .addTo(this.mapa);
+
+    this.marcadores.push({
+      color: color,
+      marcador: nuevo
+    });
+  }
 }
