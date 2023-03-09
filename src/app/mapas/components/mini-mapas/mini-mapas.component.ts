66ca4edeb70f819103908bd216e9aaea7037c515
gabopereyra <gabrielp1689@gmail.com>
2023-03-08 21:03:01 -0300

    Add structure to mini-mapa-component, add structure to propiedaes-component, in this case mini-mapa-component is added to the html to works likes a img

--- src/app/mapas/components/mini-mapas/mini-mapas.component.ts
+++ src/app/mapas/components/mini-mapas/mini-mapas.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit, ViewChild, ElementRef} from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-mini-mapas',
   templateUrl: './mini-mapas.component.html',
   styleUrls: ['./mini-mapas.component.css']
 })
-export class MiniMapasComponent implements OnInit {
+export class MiniMapasComponent implements AfterViewInit {
+  @Input() lngLat: [number, number] = [0, 0];
+  @ViewChild('mapa') divMapa! : ElementRef;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    var map = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.lngLat,
+      zoom: 15,
+      interactive: false
+    });
+
+    new mapboxgl.Marker()
+      .setLngLat(this.lngLat)
+      .addTo(map)
   }
 
 }
